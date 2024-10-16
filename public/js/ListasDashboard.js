73ccaa7a1120cd1f26da2fd4ae284d3792d29d95
MIC43LA <micaflaherty18@gmail.com>
2024-10-16 13:14:35 -0300

    Update ListasDashboard.js

--- public/js/ListasDashboard.js
+++ public/js/ListasDashboard.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     try {
-      const response = await fetch(`listas`, {
+      const response = await fetch(``, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(nuevaLista)
